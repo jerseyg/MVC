cc16a2351a9fcd0342be312a9412c57777bf96b8
Jersey Galapon <jerseyg@shaw.ca>
2013-08-02 10:30:29 -0500

    Init Commit

---
+++ mvcInvites/mvcInvites/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvcInvites.Models;
+
+namespace mvcInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Home/RsvpForm/
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //Email Response
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // validation error
+                return View();
+            }
+
+        }
+    }
+}
---
+++ mvcInvites/mvcInvites/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvcInvites.MvcApplication" Language="C#" %>
